8091750230c4cd360f7001f21352450fb49497e3
kp <kx5hu@KPMacAir.local>
2021-09-06 22:30:22 -0400

    Initial commit

---
+++ app/src/main/java/com/example/simpletodo/EditActivity.java
@@ -0,0 +1,42 @@
+package com.example.simpletodo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class EditActivity extends AppCompatActivity {
+
+    EditText edItem;
+    Button btnSave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        edItem = findViewById(R.id.edItem);
+        btnSave = findViewById(R.id.btnSave);
+
+        getSupportActionBar().setTitle("Edit Item");
+
+        edItem.setText(getIntent().getStringExtra(MainActivity.KEY_ITEM_TEXT));
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Create intent which will contain results of user's modifications
+                Intent intent = new Intent();
+
+                //Pass results of editing
+                intent.putExtra(MainActivity.KEY_ITEM_TEXT, edItem.getText().toString());
+                intent.putExtra(MainActivity.KEY_ITEM_POSITION, getIntent().getExtras().getInt(MainActivity.KEY_ITEM_POSITION));
+                //Set the result of intent
+                setResult(RESULT_OK, intent);
+                //Finish the activity, close the screen and go back
+                finish();
+            }
+        });
+    }
+}
